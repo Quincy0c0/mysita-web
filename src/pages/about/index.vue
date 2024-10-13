117540f33361786a318d99c5beda4df45c6c0ce7
Quincy <ostibiall@outlook.com>
2024-10-13 10:21:50 +0800

    (feat: updated components and moved Markdown rendering to script setup)

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -4,24 +4,20 @@
     v-html="htmlContent"></div>
 </template>
 
-<script>
+<script setup>
   import { ref, onMounted } from 'vue';
   import MarkdownIt from 'markdown-it';
-  import hljs from 'highlight.js';
-
-  const response = await fetch('/src/assets/markdown/Introduce.md'); // 替换为你的 Markdown 文件路径
-  const text = await response.text();
 
   const md = new MarkdownIt(); // 创建一个 MarkdownIt 实例
 
-  const htmlContent = ref(md.render(text));
+  const htmlContent = ref(null);
 
-  export default {
-    name: 'MarkdownViewer',
-    setup() {
-      return { htmlContent };
-    },
-  };
+  onMounted(async () => {
+    const response = await fetch('/src/assets/markdown/Introduce.md'); // 替换为你的 Markdown 文件路径
+    const text = await response.text();
+
+    htmlContent.value = md.render(text);
+  });
 </script>
 
 <style>
